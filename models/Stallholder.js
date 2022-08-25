7f872a0d0d10e57146f8ded277bd7ee7e30a59ca
nancy <smithnancy@outlook.com>
2022-08-25 21:19:47 +1000

    module stallholder

---
+++ models/Stallholder.js
@@ -0,0 +1,56 @@
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Stallholder extends Model {}
+
+Stallholder.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    company_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [0,15],
+        },
+    },
+    website: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isURL: true,
+      },
+    },
+    mobile: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+          isNumeric:true,
+          len: [0,15],
+      },
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        len: [0,32],
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'stallholder',
+  }
+);
+
+module.exports = Stallholder;
+
+
+
+/** */
--- models/index.js
+++ models/index.js
@@ -1,6 +1,9 @@
 const User = require('./User');
+const Stallholder = require('./Stallholder');
+
 
 // Belongs to and has many etc. initialisations
 
 
 module.exports = { User };
+module.exports = { Stallholder };
