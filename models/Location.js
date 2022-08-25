16cd6506189838b9819ec35c20eedad74107ae82
nancy <smithnancy@outlook.com>
2022-08-25 21:46:22 +1000

    model location created

---
+++ models/Location.js
@@ -0,0 +1,51 @@
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Location extends Model {}
+
+Location.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    marketname_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [0,32],
+        },
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len:[0,30],
+      },
+    },
+    website: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+          isUrl:true,
+      },
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        len: [0,32],
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Location',
+  }
+);
+
+module.exports = Location;
