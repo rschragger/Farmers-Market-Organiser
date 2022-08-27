80d7a7e5b937aec24d38f75a58e80dd25c9c7831
Reeve Schragger <80379457+rschragger@users.noreply.github.com>
2022-08-27 14:46:12 +1000

    Fixes

---
+++ seeds/seeds.js
@@ -0,0 +1,27 @@
+const sequelize = require('../config/connection');
+const { User, Stallholder } = require('../models');
+
+const userSeedData = require('./userSeedData.json');
+const stallholderSeedData = require('./stallholderSeedData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const user = await User.bulkCreate(userSeedData,
+    {
+      individualHooks: true,
+      returning: true,
+
+    })
+    .catch(err => console.log(err));
+
+  const stallholder = await Stallholder.bulkCreate(stallholderSeedData, {
+    individualHooks: true,
+    returning: true,
+  })
+  .catch(err => console.log(err));
+
+  process.exit(0);
+};
+
+seedDatabase();
