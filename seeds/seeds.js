faf478c67835fc8b850982543e7d3a892a95c075
Reeve Schragger <80379457+rschragger@users.noreply.github.com>
2022-08-27 18:36:56 +1000

    Added Stallholder data to the user get call

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -5,6 +5,9 @@ const userSeedData = require('./userSeedData.json');
 const stallholderSeedData = require('./stallholderSeedData.json');
 const locationSeedData = require('./locationSeedData.json');
 
+const randomId = (obj) => {
+  return obj[Math.floor(Math.random() * obj.length)].id
+}
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -15,18 +18,21 @@ const seedDatabase = async () => {
   })
     .catch(err => console.log(err));
 
-    const location = await Location.bulkCreate(locationSeedData, {
-      individualHooks: true,
-      returning: true,
-    })
-      .catch(err => console.log(err));
+  const location = await Location.bulkCreate(locationSeedData, {
+    individualHooks: true,
+    returning: true,
+  })
+    .catch(err => console.log(err));
 
   for (const user of userSeedData) {
+    let stallOrOrg = Math.floor(Math.random() * 2); //can be either a stallholder or an organiser seeding
+    let shId = (stallOrOrg === 0) ? null : randomId(stallholder);
+    let orgId = (stallOrOrg === 1) ? null : randomId(location);
     const newUser = await User.create(
       {
         ...user,
-        stallholder_id: stallholder[Math.floor(Math.random() * stallholder.length)].id,
-        location_id: location[Math.floor(Math.random() * location.length)].id,
+        stallholder_id: shId,
+        location_id: orgId,
         individualHooks: true,
         returning: true,
       })
