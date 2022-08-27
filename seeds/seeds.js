396601700744e8ceae07a5f005cdd113c916a3a7
Reeve Schragger <80379457+rschragger@users.noreply.github.com>
2022-08-27 14:07:46 +1000

    Added foreign keys to User

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -11,7 +11,7 @@ const seedDatabase = async () => {
     {
       individualHooks: true,
       returning: true,
-    } );
+    });
 
   const stallholder = await Stallholder.bulkCreate(stallholderSeedData, {
     individualHooks: true,
