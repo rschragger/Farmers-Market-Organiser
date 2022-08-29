03125992f50b81abbb38c13743f64f0cd8fc4075
Reeve Schragger <80379457+rschragger@users.noreply.github.com>
2022-08-30 07:17:07 +1000

    Added both withAuth middleware and ALSO isOrganiser, isStallholder as indicators we can use to switch between

--- models/Booking.js
+++ models/Booking.js
@@ -2,7 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Booking extends Model {}
+class Booking extends Model { }
 
 Booking.init(
   {
@@ -13,39 +13,39 @@ Booking.init(
       autoIncrement: true,
     },
     event_id: {
-        type: DataTypes.INTEGER,
-		allowNull: false,
-        references: {
-			model: 'events',
-			key: 'id',
-		},
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'events',
+        key: 'id',
+      },
     },
     stall_id: {
-        type: DataTypes.INTEGER,
-		allowNull: false,
-        references: {
-			model: 'stall',
-			key: 'id',
-		},
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'stall',
+        key: 'id',
+      },
     },
     stallholder_id: {
-        type: DataTypes.INTEGER,
-		allowNull: false,
-        references: {
-			model: 'stallholder',
-			key: 'id',
-		},
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'stallholder',
+        key: 'id',
+      },
     },
     timestamp_start: {
-        type: DataTypes.DATE,
-        allowNull: false,
+      type: DataTypes.DATE,
+      allowNull: false,
     },
     timestamp_end: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        validate: {
-            isAfter: this.timestamp_start,
-        }
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        isAfter: this.timestamp_start,
+      }
     },
   },
   {
--- utils/auth.js
+++ utils/auth.js
@@ -8,7 +8,29 @@ const withAuth = (req, res, next) => {
   }
 };
 
-module.exports = { withAuth };
+// This is not a helper as it does not have a next()
+// It is used to simply identify true or false
+const isOrganiser = (req, res ) => {
+  if (req.session.role_type === "organiser") {
+    return true
+  }
+  else {
+    return false
+  }
+};
+
+// This is not a helper as it does not have a next()
+// It is used to simply identify true or false
+const isStallholder = (req, res ) => {
+  if (req.session.role_type === "stallholder") {
+    return true
+  }
+  else {
+    return false
+  }
+};
+
+module.exports = { withAuth, isOrganiser, isStallholder };
 
 /* Might not want to use these, just make a withAuth and use res.session.isOrganiser 
 
