983ec9b7fb9eda0d7ad520452b921d37e7cdb393
Reeve Schragger <80379457+rschragger@users.noreply.github.com>
2022-08-29 17:58:43 +1000

    Login working, had to make a sequelize session

--- controllers/api/stallholderRoutes.js
+++ controllers/api/stallholderRoutes.js
@@ -3,25 +3,62 @@ const { Stallholder, User, Product  } = require('../../models'); //, Booking
 
 router.get('/', async (req, res) => {
   try {
-
     const stallholderData = await Stallholder.findAll({
       include: [{ model: User },{model:Product}],
-
     })
     if (!stallholderData) {
       res
         .status(400)
-        .json({ message: 'No data found' });
+        .json({ message: 'No data found for stallholders' });
       return;
     }
-
-    res.status(200).json(stallholderData);//{message:"Test working!"})
+    res.status(200).json(stallholderData);
   } catch (err) {
     res.status(400).json(err);
   }
+});
+
+// Get by stallholder id
+router.get('/:id', async (req, res) => {
+  try {
+    const stallholderData = await Stallholder.findAll({
+      include: [{ model: User },{model:Product}],
+      where:{ 
+        id: req.params.id}
+    })
+    if (!stallholderData) {
+      res
+        .status(400)
+        .json({ message: `No data found for id ${req.params.id}` });
+      return;
+    }
+    res.status(200).json(stallholderData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 
-})
+// Delete stallholder by id
+// This has an issue because it is a parent record
+router.delete('/:id', async (req, res) => {
+  try {
+    const stallholderData = await Stallholder.destroy({
+      where:{ 
+        id: req.params.id}
+    })
+    if (!stallholderData) {
+      res
+        .status(400)
+        .json({ message: `No data found for id ${req.params.id}, nothing deleted` });
+      return;
+    }
+    res.status(200).json({message: `Stallholder with id ${req.params.id} has been deleted`});
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
+
 const exphbs = require('express-handlebars');
 const mysql = require('mysql2');
 
 const routes = require('./controllers');
+
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
+
+// Configure and link a session object with the sequelize store
+const sess = {
+  secret: 'Farmers Market secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+// Add express-session and store as Express.js middleware
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
