2f64dec386d0c8ed90ee83a2099d93a02d68f793
Reeve Schragger <80379457+rschragger@users.noreply.github.com>
2022-08-30 19:57:50 +1000

    Added product API routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes.js');
 const locationRoutes = require('./locationRoutes.js');
 const stallholderRoutes = require('./stallholderRoutes.js');
+const productRoutes = require('./productRoutes');
 
 router.use('/users', userRoutes);
 router.use('/locations', locationRoutes);
 router.use('/stallholder', stallholderRoutes);
+router.use('/product', productRoutes);
 
 module.exports = router;
