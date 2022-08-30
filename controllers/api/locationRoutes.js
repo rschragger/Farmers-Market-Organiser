5742d71411633624caf42ec89c55dfa9de91c990
nancy <smithnancy@outlook.com>
2022-08-30 21:10:50 +1000

    location rutest fix conflict

--- controllers/api/locationRoutes.js
+++ controllers/api/locationRoutes.js
@@ -1,81 +1,31 @@
 const router = require('express').Router();
-const { Location } = require('../../models');
-
-// Retrieve all the Locations
+const { Location, User, Stall, Events, Booking } = require('../../models');
 router.get('/', async (req, res) => {
-  try {
-    const locations = await Location.findAll();
-    res.status(200).json({
-      data: locations
-    });
-  }
-  catch (err) {
-    res.status(500).json({
-      message: err
-    });
-  }
-});
+    try {
 
-// Retrieve one location
-router.get('/:id', async (req, res) => {
-  try {
-    const location = await Location.findByPk(req.params.id);
+        const locationData = await Location.findAll({
+            include: [
+                { model: User },
+                { model: Stall }, 
+                {model: Events},
+                {model: Booking},
+            ],
+            exclude:["stallholder_id"]
+        })
+        if (!locationData) {
+            res
+                .status(400)
+                .json({ message: 'No data found' });
+            return;
+        }
 
-    res.status(200).json({
-      data: location
-    });
-  }
-  catch (err) {
-    res.status(500).json({
-      message: err
-    });
-  }
-});
-
-// Create a new location
-router.post('/', async (req, res) => {
-  try {
-	const newLocation = await Location.create(req.body);
-
-	res.status(200).json({
-	data: newLocation
-	});
-  }
-  catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-// Update the Location data
-router.put('/:id', async (req, res) => {
-  try {
-    const updatedLocation = await Location.update(req.body, {
-      where: {
-        id: req.params.id
-      },
-      individualHooks: true,
-    });
-
-    if (!updatedLocation[0]) {
-      // No Location exists with this id
-      res.status(404).json({
-        message: "No Location with this id exists!"
-      });
-
-      return;
+        res.status(200).json(locationData);//{message:"Test working!"})
+    } catch (err) {
+        res.status(400).json(err);
     }
 
-    // the Location exists and has been updated
-    res.status(200).json({
-      data: updatedLocation,
-      message: "Location is updated!"
-    });
-  }
-  catch (err) {
-    res.status(500).json({
-      message: err
-    });
-  }
-});
+
+})
+
 
 module.exports = router;
