713012ab5e4079b35985361ca296f0ca44ce656b
Sean Genge <seangenge@gmail.com>
2022-08-31 14:03:49 +1000

    Added login/logout and signup feature

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -70,7 +70,7 @@ router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({
       where: {
-        email: req.body.email
+        username: req.body.username
       }
     });
 
